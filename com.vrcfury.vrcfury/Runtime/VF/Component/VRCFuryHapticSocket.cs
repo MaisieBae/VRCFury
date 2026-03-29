672c9c737dee1ee2788996136f51a64e9ec1be48
Lilani <109014684+MaisieBae@users.noreply.github.com>
2026-03-28 17:10:28 -0700

    Add files via upload

--- com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryHapticSocket.cs
+++ com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryHapticSocket.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using VF.Model;
 
-namespace VF.Component {
+namespace VF.Component
+{
     [AddComponentMenu("VRCFury/SPS Socket (VRCFury)")]
-    internal class VRCFuryHapticSocket : VRCFuryComponent {
-        public enum AddLight {
+    internal class VRCFuryHapticSocket : VRCFuryComponent
+    {
+        public enum AddLight
+        {
             None,
             Hole,
             Ring,
@@ -14,7 +17,8 @@ namespace VF.Component {
             RingOneWay
         }
 
-        public enum EnableTouchZone {
+        public enum EnableTouchZone
+        {
             Auto,
             On,
             Off
@@ -22,7 +26,6 @@ namespace VF.Component {
 
         public AddLight addLight = AddLight.Auto;
         public new string name;
-        public string oscId;
         public EnableTouchZone enableHandTouchZone2 = EnableTouchZone.Auto;
         public float length;
         public bool unitsInMeters = true;
@@ -32,7 +35,7 @@ namespace VF.Component {
         public Vector3 position;
         public Vector3 rotation;
         [NonSerialized] public bool sendersOnly = false;
-        
+
         public List<DepthActionNew> depthActions2 = new List<DepthActionNew>();
         public State activeActions = new State();
         public bool useHipAvoidance = true;
@@ -45,14 +48,15 @@ namespace VF.Component {
                                          !string.IsNullOrWhiteSpace(plugLengthParameterName);
         public bool IsValidPlugWidth => enablePlugWidthParameter &&
                                          !string.IsNullOrWhiteSpace(plugWidthParameterName);
-        
+
         [Obsolete] public bool enableDepthAnimations = false;
         [Obsolete] public List<DepthAction> depthActions = new List<DepthAction>();
         [Obsolete] public bool enableActiveAnimation = false;
-        
+
         [Serializable]
         [Obsolete]
-        public class DepthAction {
+        public class DepthAction
+        {
             [Obsolete] public State state;
             [Obsolete] public float startDistance = 0;
             [Obsolete] public float endDistance = -0.25f;
@@ -63,61 +67,84 @@ namespace VF.Component {
             [Obsolete] public float smoothing;
         }
 
-        public enum DepthActionUnits {
+        public enum DepthActionUnits
+        {
             Meters,
             Plugs,
             Local
         }
-        
+
         [Serializable]
-        public class DepthActionNew {
+        public class DepthActionNew
+        {
             public State actionSet = new State();
             public Vector2 range = new Vector2(-0.25f, 0);
             public DepthActionUnits units = DepthActionUnits.Meters;
             public bool enableSelf;
             public float smoothingSeconds = 0;
+            public bool useExitSmoothing = false;
+            public float exitSmoothingSeconds = 2f;
+            public bool useExitAnimation = false;
+            public State exitActionSet = new State();
             public bool reverseClip = false;
         }
 
-        public override bool Upgrade(int fromVersion) {
+        public override bool Upgrade(int fromVersion)
+        {
 #pragma warning disable 0612
-            if (fromVersion < 1) {
-                if (name.Contains("D P S")) {
+            if (fromVersion < 1)
+            {
+                if (name.Contains("D P S"))
+                {
                     name = "";
                 }
             }
-            if (fromVersion < 2) {
-                foreach (var a in depthActions) {
+            if (fromVersion < 2)
+            {
+                foreach (var a in depthActions)
+                {
                     a.smoothing = 0;
                 }
             }
-            if (fromVersion < 3) {
-                foreach (var a in depthActions) {
+            if (fromVersion < 3)
+            {
+                foreach (var a in depthActions)
+                {
                     if (a.maxDepth <= a.minDepth) a.maxDepth = 0.25f;
                     a.startDistance = -a.minDepth;
                     a.endDistance = -a.maxDepth;
                 }
             }
-            if (fromVersion < 4) {
-                foreach (var a in depthActions) {
+            if (fromVersion < 4)
+            {
+                foreach (var a in depthActions)
+                {
                     a.smoothing = (float)Math.Pow(a.smoothing, 0.2);
                 }
             }
-            if (fromVersion < 5) {
-                foreach (var a in depthActions) {
+            if (fromVersion < 5)
+            {
+                foreach (var a in depthActions)
+                {
                     a.smoothingSeconds = UpgradeFromLegacySmoothing(a.smoothing);
                 }
             }
-            if (fromVersion < 6) {
+            if (fromVersion < 6)
+            {
                 enableDepthAnimations = depthActions.Count > 0;
             }
-            if (fromVersion < 7) {
+            if (fromVersion < 7)
+            {
                 enableActiveAnimation = activeActions.actions.Count > 0;
             }
-            if (fromVersion < 8) {
-                if (enableDepthAnimations) {
-                    foreach (var a in depthActions) {
-                        depthActions2.Add(new DepthActionNew() {
+            if (fromVersion < 8)
+            {
+                if (enableDepthAnimations)
+                {
+                    foreach (var a in depthActions)
+                    {
+                        depthActions2.Add(new DepthActionNew()
+                        {
                             actionSet = a.state,
                             enableSelf = a.enableSelf,
                             range = new Vector2(
@@ -131,7 +158,8 @@ namespace VF.Component {
                     }
                 }
                 depthActions.Clear();
-                if (!enableActiveAnimation) {
+                if (!enableActiveAnimation)
+                {
                     activeActions.actions.Clear();
                 }
             }
@@ -139,15 +167,18 @@ namespace VF.Component {
             return false;
         }
 
-        public static float UpgradeFromLegacySmoothing(float oldSmoothingVal) {
+        public static float UpgradeFromLegacySmoothing(float oldSmoothingVal)
+        {
             if (oldSmoothingVal == 0) return 0;
             return GetFramesRequired((float)(1 - Math.Pow(oldSmoothingVal, 0.1)), true) / 60f;
         }
-        public static int GetFramesRequired(float fractionPerFrame, bool useAcceleration) {
-            var targetFraction = 0.9f; // Let's say 90% is enough to be considered "done"
+        public static int GetFramesRequired(float fractionPerFrame, bool useAcceleration)
+        {
+            var targetFraction = 0.9f;
             float target = useAcceleration ? 0 : 1;
             float position = 0;
-            for (var frame = 1; frame < 1000; frame++) {
+            for (var frame = 1; frame < 1000; frame++)
+            {
                 target += (1 - target) * fractionPerFrame;
                 position += (target - position) * fractionPerFrame;
                 if (position >= targetFraction) return frame;
@@ -155,7 +186,8 @@ namespace VF.Component {
             return 1000;
         }
 
-        public override int GetLatestVersion() {
+        public override int GetLatestVersion()
+        {
             return 8;
         }
     }
